4769b2d79a8d2064db1a9b9a49dce6ccd0fdfef7
filer358 <filer358@student.liu.se>
2017-10-12 14:46:52 +0200

    change wsgi

--- wsgi.py
+++ wsgi.py
@@ -291,7 +291,7 @@ $ git push</pre>
 # Below for testing only
 #
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
+    from main.py import app as application
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
     httpd.handle_request()
