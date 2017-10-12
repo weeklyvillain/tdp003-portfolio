c603d6fe05bd48a4ed02f7bbfb27d4f168978f8c
filer358 <filer358@student.liu.se>
2017-10-12 15:09:35 +0200

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import os
 
-
-virtualenv = 'bin/activate_this.py'
+virtenv = os.environ['var/lib/openshift/59df606679c0532a7c000576/app-root/runtime/repo'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     with open(virtualenv) as f:
         code = compile(f.read(), virtualenv, 'exec')
@@ -13,7 +14,7 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
+import os
+os.chdir(os.environ['var/lib/openshift/59df606679c0532a7c000576/app-root/'])
 
-
-from main import app as application
-
+from server import app as application
